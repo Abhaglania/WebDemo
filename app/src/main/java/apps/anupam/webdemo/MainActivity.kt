d07e244fd188ac1f25a8085752d14eed9506d8a3
Anupam <anupamb@brdsys.com>
2018-01-05 12:31:38 +0530

    adding internet permission and setting up webview

--- app/src/main/java/apps/anupam/webdemo/MainActivity.kt
+++ app/src/main/java/apps/anupam/webdemo/MainActivity.kt
@@ -1,12 +1,18 @@
 package apps.anupam.webdemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val webView = findViewById<WebView>(R.id.webView);
+        val webSettings = webView.settings;
+        webSettings.javaScriptEnabled = true;
+        val url = "http://192.168.1.148/anupam/demo/";
+        webView.loadUrl(url)
     }
 }
---
+++ app/src/main/java/apps/anupam/webdemo/WebAppInterface.kt
@@ -0,0 +1,5 @@
+package apps.anupam.webdemo
+
+/**
+ * Created by pc4 on 05-Jan-18.
+ */
