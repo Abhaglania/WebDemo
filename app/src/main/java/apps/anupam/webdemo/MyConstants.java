eed6ab80ea242a807aba176d284de3ef0725911c
Anupam <anupamb@brdsys.com>
2018-01-05 16:54:38 +0530

    minor fixes

--- app/src/main/java/apps/anupam/webdemo/MyConstants.java
+++ app/src/main/java/apps/anupam/webdemo/MyConstants.java
@@ -2,6 +2,6 @@ package apps.anupam.webdemo;
 
 
 public class MyConstants {
-    static final String host = "http://192.168.1.148";
+    static final String host = "192.168.1.148";
     static final String url = "http://192.168.1.148/anupam/demo";
 }
