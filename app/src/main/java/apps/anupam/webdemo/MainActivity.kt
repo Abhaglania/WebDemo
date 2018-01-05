d0bf1b9a09151d471fd4cb5165cdd66ac567babe
Anupam <anupamb@brdsys.com>
2018-01-05 12:47:42 +0530

    linking weppage with android app using webappinterface

--- app/src/main/java/apps/anupam/webdemo/MainActivity.kt
+++ app/src/main/java/apps/anupam/webdemo/MainActivity.kt
@@ -3,17 +3,23 @@ package apps.anupam.webdemo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val webView = findViewById<WebView>(R.id.webView)
-        val webSettings = webView.settings
-        webSettings.javaScriptEnabled = true
         val webAppInterface = WebAppInterface(this)
         webView.addJavascriptInterface(webAppInterface, "Android")
+
+        webView.webViewClient= WebViewClient()
+
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+
         val url = "http://192.168.1.148/anupam/demo";
         webView.loadUrl(url)
     }
