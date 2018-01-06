c7032279b23864383f3ecece7b837f1afed09954
abhaglania <anupambhaglania@gmail.com>
2018-01-06 22:41:41 +0530

    fixes and url changed

--- app/src/main/java/apps/anupam/webdemo/MainActivity.kt
+++ app/src/main/java/apps/anupam/webdemo/MainActivity.kt
@@ -3,29 +3,39 @@ package apps.anupam.webdemo
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
 import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
-      var host="http://192.168.1.148";
+    var host = "http://192.168.1.148";
+    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val webView = findViewById<WebView>(R.id.webView)
+        webView = findViewById<WebView>(R.id.webView)
 
         val webAppInterface = WebAppInterface(this)
         webView.addJavascriptInterface(webAppInterface, "Android")
 
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-            webView.webViewClient = MyWebViewClientAboveLOLLIPOP(this)
-        else
-            webView.webViewClient = MyWebViewClient(this);
+        webView.webViewClient = MyWebViewClient(this);
 
         val webSettings = webView.settings
         webSettings.javaScriptEnabled = true
 
 
         webView.loadUrl(MyConstants.url)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event)
     }
 }
--- app/src/main/java/apps/anupam/webdemo/MyConstants.java
+++ app/src/main/java/apps/anupam/webdemo/MyConstants.java
@@ -2,6 +2,6 @@ package apps.anupam.webdemo;
 
 
 public class MyConstants {
-    static final String host = "192.168.1.148";
-    static final String url = "http://192.168.1.148/anupam/demo";
+    static final String host = "192.168.1.8";
+    static final String url = "http://192.168.1.8/anupam/demo/index.html";
 }
